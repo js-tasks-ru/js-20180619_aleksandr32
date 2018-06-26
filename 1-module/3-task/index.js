0eec1ab88ca2db97c4bd1d1c707ad45de973b168
Alex <alex23-12@mail.ru>
2018-06-26 13:46:50 +0300

    Решение задачи 1.3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,11 +1,27 @@
-'use strict';
+"use strict";
 
-/**
- * Найти min/max в произвольной строке
- * @param   {string} str -  входные данные
- * @returns {{min:number, max:number}}  объект
- */
-function getMinMax(str) {
+let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
 
+let arr = inputData.split(' ');
+let arr2 = [];
+let arr3 = [];
+
+for (let i= 0; i< arr.length; i++) {
+  arr2 = arr[i].split(',');
+
+  for (let q= 0; q< arr2.length; q++) {
+  	arr2[q] = parseFloat(arr2[q]);
+  	arr3.push(arr2[q]);
+  	} 	
 }
 
+let positiveArr = arr3.filter(function(number) {
+  return number;
+});
+
+function getMinMax() {
+  let max = Math.max.apply(Math,positiveArr);
+  let min = Math.min.apply(Math,positiveArr);
+  return {min : min, max : max};
+}
+console.log(getMinMax());
