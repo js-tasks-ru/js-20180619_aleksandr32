bbc6595c08b3b80a0e11d62242dcdac2c074edc7
Alex <alex23-12@mail.ru>
2018-07-04 17:24:30 +0300

    Решение задачи 2.1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,16 @@
  * @returns {Object}
  */
 function clone (obj) {
+    if (obj === null) {
+        return null;
+    }
+
+    let objclone = Object.assign({}, obj);
+    for (let key in objclone) {
+        if (typeof objclone[key] === 'object' && obj !== null) {
+            objclone[key] = clone(objclone[key]);
+        }
+    }
+
+    return objclone;
 }
