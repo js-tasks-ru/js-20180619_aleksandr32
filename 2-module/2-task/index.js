da5447efbbdd989e0157ba156d5327da1feaa0b7
Alex <alex23-12@mail.ru>
2018-07-04 17:28:51 +0300

    Решение задачи 2.2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,8 +1,28 @@
+"use strict";
 /**
  * Клонируем объект
  * @param {Object} obj - объект в котором ищем
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
+
 function find (obj, value) {
+    let arr = [];
+    let way = '';
+    function search (obj, value) {
+        for (let key in obj){
+            if (obj[key] === value) {
+                way += key;
+                arr.push(way);
+                way = '';
+            }
+            if (typeof(obj[key]) == 'object') {
+                way += key + '.';
+                search(obj[key], value);
+            }
+        }
+        if (arr.length == 0) return null;
+        return (arr.length > 1) ? arr : arr[0];
+    }
+    return search(obj, value);
 }
