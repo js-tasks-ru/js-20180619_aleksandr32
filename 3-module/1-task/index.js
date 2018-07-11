23e0fb20060258364af2dc0638cedc4b3c51ad35
Alexandr <alex23-12@mail.ru>
2018-07-11 20:27:01 +0300

    Решение задачи 3.1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,6 +5,12 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+  let result = [];
+  data.forEach(function (user) {
+    if (user.age <= age) {
+      result.push(`${user.name}, ${user.balance}`);
+    }
+  });
+
+  return result.join('\n');
 }
-
-
