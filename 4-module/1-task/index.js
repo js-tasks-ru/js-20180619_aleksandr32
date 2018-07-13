1d2cceb4be9822fde54f3b9aa5cf27988f24d7f8
Alexandr <alex23-12@mail.ru>
2018-07-13 19:20:09 +0300

    Решение задачи 4.1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+    let ul = document.createElement('ul');
+
+    for(let key of friends) {
+        let li = document.createElement('li');
+        li.innerHTML = `${key.firstName} ${key.lastName}`;
+        ul.appendChild(li);
+    }
+    return ul;
 }
