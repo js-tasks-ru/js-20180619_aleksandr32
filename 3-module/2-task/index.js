0012f2b4815356a9d7451bb05b8057237b9a1a37
Alexandr <alex23-12@mail.ru>
2018-07-16 16:52:09 +0300

    Решение задачи 3.2

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,9 +1,33 @@
+'use strict'
+
 let calendar = {
     from: new Date('2018-01-01T15:09:10Z'),
     to: new Date('2018-02-01T10:09:10Z')
 };
 
 calendar[Symbol.iterator] = function () {
+    let current = new Date(this.from);
+    const last = this.to;
+
+    return {
+        next() {
+            if (current <= last) {
+                current.setDate(current.getDate() + 1);
+                let currentDay = current.getDay();
+                let currentDate = ('0' + current.getDate()).substr(-2);
+                return {
+                    value: (currentDay === 0 || currentDay === 6) ? `[${currentDate}]` : currentDate,
+                    done: false
+                }
+            }
+            return {done: true};
+        }
+    }
 };
+
+for (let day of calendar) {
+    console.log(day);
+}
+
 
 
